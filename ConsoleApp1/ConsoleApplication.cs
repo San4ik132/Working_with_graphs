b43c9a8d682c95d13fdaa70b981f68f5c140a6ed
San4ik132 <99422931+San4ik132@users.noreply.github.com>
2023-10-12 00:07:46 +0400

    Add files via upload
    Lab1

---
+++ ConsoleApp1/ConsoleApplication.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public class ConsoleApplication
+    {
+        public string choice3 = string.Empty;
+        public string choice4 = string.Empty;
+        Graph graph1 = new Graph();
+        public void RUN()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            bool isRunning1 = true;
+            bool isRunning2 = true;
+            bool isRunning3 = true;
+            bool isRunning4 = true;
+            Console.Write("ДОБРО ПОЖАЛОВАТЬ В ПРИЛОЖЕНИЕ РАБОТЫ С ГРАФАМИ\n");
+
+            while (isRunning1)
+            {
+
+                while (isRunning2)
+                {
+
+                    while (isRunning3)
+                    {
+                        
+                        while (isRunning4)
+                        {
+                            Console.Write("Определите с каким графом работать: 0 - направленный; 1 - ненаправленный\n");
+                            Console.WriteLine("Ваш выбор");
+                            string choice1 = Console.ReadLine();
+                            switch (choice1)
+                            {
+                                case "0":
+                                    choice3 = choice1;
+                                    isRunning4 = false;
+                                    break;
+                                case "1":
+                                    choice3 = choice1;
+                                    isRunning4 = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("Неверный выбор! Пробовать снова.");
+                                    break;
+                            }
+                        }
+
+                        Console.Write("Определите с каким графом работать: 0 - взвешенный; 1 - невзвешенный\n");
+                        Console.WriteLine("Ваш выбор");
+                        string choice2 = Console.ReadLine();
+                        switch (choice2)
+                        {
+                            case "0":
+                                choice4 = choice2;
+                                isRunning3 = false;
+                                break;
+                            case "1":
+                                choice4 = choice2;
+                                isRunning3 = false;
+                                break;
+                            default:
+                                Console.WriteLine("Неверный выбор! Пробовать снова.");
+                                break;
+                        }
+
+                    }
+
+                    Thread.Sleep(60);
+                    Console.WriteLine("1. Добавить вершину");
+                    Thread.Sleep(60);
+                    Console.WriteLine("2. Добавить связь");
+                    Thread.Sleep(60);
+                    Console.WriteLine("3. Удалить вершину");
+                    Thread.Sleep(60);
+                    Console.WriteLine("4. Удалить связь");
+                    Thread.Sleep(60);
+                    Console.WriteLine("5. Ввывод в консоль графа");
+                    Thread.Sleep(60);
+                    Console.WriteLine("6. Загрузить граф из файла");
+                    Thread.Sleep(60);
+                    Console.WriteLine("7. Печать граф в файл");
+                    Thread.Sleep(60);
+                    Console.WriteLine("8. Создать новый граф");
+                    Thread.Sleep(60);
+                    Console.WriteLine("9. Выход");
+                    Thread.Sleep(60);
+                    Console.Write("Введите свой выбор: ");
+                    string choice = Console.ReadLine();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            AddVertexMenu();
+                            break;
+                        case "2":
+                            if (choice3 == "0" ) AddEdgeMenu();
+                            else AddEdgeUndirectedMenu();
+                            break;
+                        case "3":
+                            RemoveVertexMenu();
+                            break;
+                        case "4":
+                            if (choice3 == "0") RemoveEdgeMenu();
+                            else RemoveEdgeUndirectedMenu();
+                            break;
+                        case "5":
+                            graph1.PrintG(choice3, choice4);
+                            break;
+                        case "6":
+                            DownloadFromFileMenu();
+                            break;
+                        case "7":
+                            graph1.PrintToFILE(choice3, choice4);
+                            break;
+                        case "8":
+                            graph1.Clear();
+                            isRunning4 = true;
+                            isRunning3 = true;
+                            isRunning2 = true;
+                            break;
+                        case "9":
+                            graph1.Clear();
+                            isRunning4 = false;
+                            isRunning3 = false;
+                            isRunning2 = false;
+                            isRunning1 = false;
+                            break;
+                        default:
+                            Console.WriteLine("Неверный выбор! Пробовать снова.");
+                            break;
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+        }
+
+    
+
+
+
+        // Добавление вершины
+        private void AddVertexMenu()
+        {
+            Console.Write("Введите название вершины: ");
+            string vertex = Console.ReadLine();
+            if (graph1.AddVertex(vertex.Trim()) == 1)
+            {
+                Console.WriteLine("Вершина успешно добавленна!");
+            }
+            else
+            {
+                Console.WriteLine("Такая вершина уже существует или введена не верно");
+            }
+            
+        }
+        // Удаление вершины
+        private void RemoveVertexMenu()
+        {
+            Console.Write("Введите название вершины: ");
+            string vertex = Console.ReadLine();
+            if (graph1.RemoveVertex(vertex.Trim()) == 1)
+            {
+                Console.Write("Вершина удалена");
+            }
+            else
+            {
+                Console.WriteLine("Вершина не может быть удалена так как она не существует или введена не коректно");
+            }
+
+        }
+
+        // Добавления ребра для направленного графа
+        private void AddEdgeMenu()
+        {
+            Console.Write("Введите начальную вершину: ");
+            string source = Console.ReadLine();
+            Console.Write("Введите конечную вершину: ");
+            string target = Console.ReadLine();
+            string weight = string.Empty;
+            if (choice4 == "0")
+            {
+                Console.Write("Введите вес ");
+                weight = Console.ReadLine().Trim();
+            }
+
+            if (weight == string.Empty && int.TryParse(weight, out int result) == false)
+            {
+                weight = "0";
+            }
+            if (graph1.AddEdge(source.Trim(), target.Trim(), weight.Trim()) == 1) 
+            {
+                Console.WriteLine("Связь успешно добавленна");
+            }
+            else
+            {
+                Console.WriteLine("Такая связь уже существует, или введена не корректно");
+            }
+            
+        }
+
+       
+        // Добавления ребра для неправленного графа
+        private void AddEdgeUndirectedMenu()
+        {
+            Console.Write("Введите начальную вершину: ");
+            string source = Console.ReadLine();
+            Console.Write("Введите конечную вершину: ");
+            string target = Console.ReadLine();
+
+            string weight = string.Empty;
+            if (choice4 == "0")
+            {
+                Console.Write("Введите вес ");
+                weight = Console.ReadLine().Trim();
+            }
+
+            if (weight == string.Empty && int.TryParse(weight, out int result) == false)
+            {
+                weight = "0";
+            }
+            if (graph1.AddEdgeUndirected(source.Trim(), target.Trim(), weight.Trim()) == 1)
+            {
+                Console.WriteLine("Связь успешно добавленна");
+            }
+            else
+            {
+                Console.WriteLine("Такая связь уже существует, или введена не корректно");
+            }
+
+        }
+
+        // Удаление ребра для направленного
+        private void RemoveEdgeMenu() 
+        {
+            Console.Write("Введите начальную вершину: ");
+            string source = Console.ReadLine();
+            Console.Write("Введите конечную вершину: ");
+            string target = Console.ReadLine();
+            if (graph1.RemoveEdge(source.Trim(), target.Trim()) == 1)
+            {
+                Console.WriteLine("Связь успешно удалена");
+            }
+            else
+            {
+                Console.WriteLine("Cвязь введена не корректно или не существуе");
+            }
+
+        }
+        // Удаление ребра для ненаправленного
+        private void RemoveEdgeUndirectedMenu()
+        {
+            Console.Write("Введите начальную вершину: ");
+            string source = Console.ReadLine();
+            Console.Write("Введите конечную вершину: ");
+            string target = Console.ReadLine();
+            if (graph1.RemoveEdgeUndirected(source.Trim(), target.Trim()) == 1)
+            {
+                Console.WriteLine("Связь успешно удалена");
+            }
+            else
+            {
+                Console.WriteLine("Cвязь введена не корректно или не существуе");
+            }
+        }
+
+        private void DownloadFromFileMenu()
+        {
+            Console.Write("Введите имя файла: ");
+            string FileName = Console.ReadLine();
+            (string line1, string line2) = graph1.DownloadFromFile(FileName);
+            choice3 = line1;
+            choice4 = line2;
+        }
+    }
+}
---
+++ ConsoleApp1/Graph.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+
+namespace ConsoleApp1
+{
+    public class Graph
+    {
+        public Dictionary<string, List<string>> MyGraph;
+        
+
+        // Конструктор по умолчанию, создающий пустой граф
+
+        public Graph()
+        {
+            MyGraph = new Dictionary<string, List<string>>();
+        }
+        // Конструктор чтения файла
+        public Graph(string FileName)
+        {
+            FileReading(FileName);
+        }
+
+        // Конструктор копирования
+        public Graph(Graph other)
+        {
+            foreach (string vertex in other.MyGraph.Keys)
+            {
+                List<string> edges = other.MyGraph[vertex];
+                MyGraph.Add(vertex, new List<string>(edges));
+            }
+        }
+
+        // Методы чтение файла MyGraph
+
+        public void FileReading (string FileName)
+        {
+            MyGraph = new Dictionary<string, List<string>>();
+            try
+            { 
+                string[] lines = File.ReadAllLines(FileName);
+                foreach (string line in lines)
+                {
+                   
+                    string[] tokens = line.Split(' ');
+                    string vertex = tokens[0];
+                    List<string> edges = new List<string>();
+
+                    for (int i = 1; i < tokens.Length; i++)
+                    {
+                        edges.Add(tokens[i]);
+                    }
+
+                    MyGraph.Add(vertex, edges);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Файл с таким именем '{FileName}' не найден");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Ошибка ввода-вывода");
+            }
+        }
+
+
+       // Добавление вершины
+        public int AddVertex(string vertex)
+        {
+            if (!MyGraph.ContainsKey(vertex) && vertex != "" && vertex != " ")
+            {
+                MyGraph.Add(vertex, new List<string>());
+                return 1;
+            }        
+            return 0;            
+        }
+        // Удаление вершины
+        public int RemoveVertex(string vertex) 
+        {
+            if (MyGraph.ContainsKey(vertex))
+            {
+                MyGraph.Remove(vertex);
+
+                // Создаем временный список для хранения удаляемых ребер
+                List<string> edgesToRemove = new List<string>();
+
+                // Ищем и добавляем удаляемые ребра во временный список
+                foreach (var kvp in MyGraph)
+                {
+                    var edges = kvp.Value;
+                    if (edges.Contains(vertex))
+                    {
+                        edgesToRemove.Add(kvp.Key);
+                    }
+                }
+
+                // Удаляем ребра из словаря
+                foreach (string key in edgesToRemove)
+                {
+                    MyGraph[key].Remove(MyGraph[key][MyGraph[key].IndexOf(vertex) + 1]);
+                    MyGraph[key].Remove(vertex);
+                }
+
+                return 1;
+            }
+
+            return 0;
+        }
+
+        // Добавление связи для направленного
+        public int AddEdge(string source, string target, string weight)
+        {
+            if (MyGraph.ContainsKey(source) && MyGraph.ContainsKey(target))
+            {
+                foreach (var K in MyGraph)
+                {
+                    foreach (var V in K.Value)
+                    {
+                        if (target == V && source == K.Key) return 0;
+                    }
+                }
+                List<string> sourceEdges = MyGraph[source];
+                sourceEdges.Add(target);
+                sourceEdges.Add(weight);
+                return 1;
+            }
+            return 0;
+
+        }
+
+        // Добавление связи для не направленного
+        public int AddEdgeUndirected(string source, string target, string weight)
+        {
+            if (MyGraph.ContainsKey(source) && MyGraph.ContainsKey(target))
+            {
+                var sourceEdges = MyGraph[source];
+                var targetEdges = MyGraph[target];
+
+                if (!sourceEdges.Contains(target) && !targetEdges.Contains(source))
+                {
+                    sourceEdges.Add(target);
+                    sourceEdges.Add(weight);
+                    targetEdges.Add(source);
+                    targetEdges.Add(weight);
+                    return 1;
+                }
+            }
+            return 0;
+        }
+
+        // Удаление связи для направленного 
+        public int RemoveEdge(string source, string target)
+        {
+            if (MyGraph.ContainsKey(source) && MyGraph.ContainsKey(target))
+            {
+                var sourceEdges = MyGraph[source];
+
+                if (sourceEdges.Contains(target))
+                {
+                    sourceEdges.Remove(sourceEdges[sourceEdges.IndexOf(target) + 1]);
+                    sourceEdges.Remove(target);
+                    return 1;
+                }
+            }
+            return 0;
+        }
+
+        // Удаление связи для ненаправленного
+        public int RemoveEdgeUndirected(string source, string target)
+        {
+            if (MyGraph.ContainsKey(source) && MyGraph.ContainsKey(target))
+            {
+                var sourceEdges = MyGraph[source];
+                var targetEdges = MyGraph[target];
+
+                if (sourceEdges.Contains(target) && targetEdges.Contains(source))
+                {
+                    sourceEdges.RemoveAt(sourceEdges.IndexOf(target) + 1);
+                    sourceEdges.Remove(target);
+                    targetEdges.RemoveAt(targetEdges.IndexOf(source) + 1);
+                    targetEdges.Remove(source);                  
+                    return 1;
+                }
+            }
+            return 0;
+        }
+
+        // Печать в файл
+        public void PrintToFILE(string focus, string weight)
+        {
+         
+            string fileName = "SaveMyGraph.txt";
+
+            WriteDictionaryToFile(MyGraph, fileName, focus, weight);
+        }
+        // Доп метод печати
+        public static void WriteDictionaryToFile(Dictionary<string, List<string>> dictionary, string fileName, string focus, string weight)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName, true))
+                {
+                    writer.WriteLine("|");
+                    if (focus == "0") writer.WriteLine("Направленный");
+                    else writer.WriteLine("Ненаправленный");
+                    if (weight == "0") writer.WriteLine("Взвешенный");
+                    else writer.WriteLine("Невзвешенный");
+                    writer.Write("Вершины графа: ");
+                    foreach (var K in dictionary)
+                    {
+                        writer.Write($"{K.Key} ");
+                    }
+                    writer.WriteLine();
+                    writer.WriteLine("Список смежности");
+                    foreach (KeyValuePair<string, List<string>> entry in dictionary)
+                    {
+                        List<string> values = entry.Value;
+                        foreach (string value in values)
+                        {
+                            if (int.TryParse(value, out int result) == true)
+                            {
+                                writer.Write($"вес({result}) ");
+                            }
+                            else
+                            {
+                                writer.Write($"{entry.Key} -> {value} ");
+                            }
+                        }
+                        writer.WriteLine();
+                    }
+
+                    int size = dictionary.Count;
+                    int[,] adjacencyMatrix = new int[size, size];
+
+                    List<string> vertices = dictionary.Keys.ToList();
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            if (dictionary[vertices[i]].Contains(vertices[j]))
+                            {
+                                adjacencyMatrix[i, j] = 1;
+                            }
+                            else
+                            {
+                                adjacencyMatrix[i, j] = 0;
+                            }
+                        }
+                    }
+                    writer.WriteLine("Матрица смежности");
+                    writer.Write("  ");
+                    foreach (var vertex in vertices)
+                    {
+                        writer.Write($"{vertex} ");
+                    }
+                    writer.WriteLine();
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        writer.Write($"{vertices[i]} ");
+                        for (int j = 0; j < size; j++)
+                        {
+                            writer.Write(adjacencyMatrix[i, j] + " ");
+                        }
+                        writer.WriteLine();
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка {e.Message}");
+            }
+        }
+
+        public (string,string) DownloadFromFile(string FileName)
+        {
+            MyGraph = new Dictionary<string, List<string>>();
+            string p = string.Empty;
+            try
+            {
+                
+                string[] lines2 = File.ReadAllLines(FileName).Take(2).ToArray();
+                string[] lines = File.ReadAllLines(FileName).Skip(2).ToArray();
+                string line1 = lines2[0].Trim();
+                string line2 = lines2[1].Trim();
+                foreach (var i in lines)
+                {
+                    string[] line = i.Split(' ');
+                    string Key = line[0];
+                    List<string> diff = new List<string>();
+
+                    for (var j = 1; j < line.Length; j++)
+                    {
+                        diff.Add(line[j].Trim());
+                    }
+
+                    MyGraph.Add(Key, diff);
+                }
+
+                return (line1, line2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+               
+            }
+            return (p, p);
+        }
+
+        // Вывод в консоль
+        public void PrintG(string focus, string weight)
+        {
+            if (focus == "0") Console.WriteLine("Направленный");
+            else Console.WriteLine("Ненаправленный");
+            if (weight == "0") Console.WriteLine("Взвешенный");
+            else Console.WriteLine("Невзвешенный");
+
+            Console.Write("Вершины графа: ");
+            foreach (var K in MyGraph)
+            {
+                Console.Write($"{K.Key} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Список смежности");
+            foreach (var K in MyGraph)
+            {             
+                foreach (var V in K.Value)
+                {
+                    if(int.TryParse(V, out int result) == true)
+                    {
+                        Console.Write($"вес({result}) ");
+                    }
+                    else
+                    {
+                        Thread.Sleep(60);
+                        Console.Write($"{K.Key} -> {V} ");
+                    }
+                    
+                }
+                Console.WriteLine();   
+            }
+
+            int size = MyGraph.Count;
+            int[,] adjacencyMatrix = new int[size, size];
+
+            List<string> vertices = MyGraph.Keys.ToList();
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (MyGraph[vertices[i]].Contains(vertices[j]))
+                    {
+                        adjacencyMatrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        adjacencyMatrix[i, j] = 0;
+                    }
+                }
+            }
+            Console.WriteLine("Матрица смежности");
+            Console.Write("  ");
+            foreach (var vertex in vertices)
+            {
+                Console.Write($"{vertex} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"{vertices[i]} ");
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(adjacencyMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        // Очищение класс
+        public void Clear()
+        {
+            MyGraph = new Dictionary<string, List<string>>();
+        }
+    }
+
+}
